982af2f2c52557e24f51ec9dae3c8fdcfbbccba6
Patrik Elfström <me@patrikelfstrom.se>
2019-10-21 23:29:35 +0200

    Testing es5 syntax

--- src/lambda/restaurants-add.js
+++ src/lambda/restaurants-add.js
@@ -1,42 +1,59 @@
-import faunadb from 'faunadb';
+'use strict';
 
-const query = faunadb.query;
-const client = new faunadb.Client({
+var faunadb = require('faunadb');
+
+var query = faunadb.query;
+var client = new faunadb.Client({
   secret: process.env.FAUNADB_SERVER_SECRET
 });
 
-export const handler = async event => {
-  let restaurants = JSON.parse(event.body);
+exports.handler = async function(event) {
+  try {
+    var restaurants = JSON.parse(event.body);
+  } catch (error) {
+    console.log('Invalid JSON');
+    return {
+      statusCode: 400,
+      body: ''
+    };
+  }
 
   // Make data to an array if not an array
   if (!Array.isArray(restaurants)) {
     restaurants = [restaurants];
   }
 
-  return new Promise((resolve, reject) => {
+  return new Promise(function(resolve, reject) {
     return Promise.all(
-      restaurants.map(async restaurant => {
+      restaurants.map(async function(restaurant) {
         return client
           .query(
-            query.Create(query.Ref('classes/Restaurant'), { data: restaurant })
+            query.Create(query.Ref('classes/Restaurant'), {
+              data: restaurant
+            })
           )
-          .then(response => {
+          .then(function(response) {
             console.log('success', response);
-
             return {
               statusCode: 200,
               body: JSON.stringify(response)
             };
           })
-          .catch(error => {
+          .catch(function(error) {
             console.error(error);
-
             return {
               statusCode: 400,
               body: JSON.stringify(error)
             };
           });
       })
-    ).then(result => resolve(result), error => reject(error));
+    ).then(
+      function(result) {
+        return resolve(result);
+      },
+      function(error) {
+        return reject(error);
+      }
+    );
   });
 };
