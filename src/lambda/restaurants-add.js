0475f7c1248a73807f4eb0caa2602e9a9122c162
Patrik Elfström <me@patrikelfstrom.se>
2019-10-21 23:42:51 +0200

    test

--- src/lambda/restaurants-add.js
+++ src/lambda/restaurants-add.js
@@ -1,59 +1,31 @@
-'use strict';
+const { Client, query: q } = require('faunadb');
+const db = new Client({ secret: process.env.FAUNADB_SERVER_SECRET });
 
-var faunadb = require('faunadb');
+exports.handler = async event => {
+  if (event.httpMethod !== 'POST') {
+    return { statusCode: 405, body: 'Method Not Allowed' };
+  }
 
-var query = faunadb.query;
-var client = new faunadb.Client({
-  secret: process.env.FAUNADB_SERVER_SECRET
-});
+  const restaurants = JSON.parse(event.body);
 
-exports.handler = async function(event) {
   try {
-    var restaurants = JSON.parse(event.body);
-  } catch (error) {
-    console.log('Invalid JSON');
+    await restaurants.map(async function(restaurant) {
+      await db.query(
+        q.Create(q.Ref('classes/Restaurant'), {
+          data: restaurant
+        })
+      );
+    });
+
     return {
-      statusCode: 400,
-      body: ''
+      statusCode: 200,
+      body: JSON.stringify('success')
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify(error)
     };
   }
-
-  // Make data to an array if not an array
-  if (!Array.isArray(restaurants)) {
-    restaurants = [restaurants];
-  }
-
-  return new Promise(function(resolve, reject) {
-    return Promise.all(
-      restaurants.map(async function(restaurant) {
-        return client
-          .query(
-            query.Create(query.Ref('classes/Restaurant'), {
-              data: restaurant
-            })
-          )
-          .then(function(response) {
-            console.log('success', response);
-            return {
-              statusCode: 200,
-              body: JSON.stringify(response)
-            };
-          })
-          .catch(function(error) {
-            console.error(error);
-            return {
-              statusCode: 400,
-              body: JSON.stringify(error)
-            };
-          });
-      })
-    ).then(
-      function(result) {
-        return resolve(result);
-      },
-      function(error) {
-        return reject(error);
-      }
-    );
-  });
 };
