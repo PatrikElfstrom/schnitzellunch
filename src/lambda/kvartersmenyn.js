deae281a2ebc692005d15bf077a78e1d3f8d1b54
Patrik Elfström <me@patrikelfstrom.se>
2019-10-26 01:27:06 +0200

    Added some trimming and html decoding to normalize åäö

--- src/lambda/kvartersmenyn.js
+++ src/lambda/kvartersmenyn.js
@@ -1,19 +1,24 @@
 import axios from 'axios';
 import cheerio from 'cheerio';
+import { decode } from 'he';
 
 const parseHTML = async ({ data }, weekDay) => {
   const $ = cheerio.load(data);
   const restaurants = [];
 
   $('#lista .panel').each((index, restaurant) => {
-    const title = $('.name .t_lunch', restaurant).text();
+    const title = decode(
+      $('.name .t_lunch', restaurant)
+        .text()
+        .trim()
+    );
 
     let { address, phone } = $('.divider .name', restaurant)
       .text()
       .match(/ADRESS: (?<address>.+?)TEL: (?<phone>.+?)(?= {2})/s).groups;
 
     // Remove more than two spaces whitespace
-    address = address.replace(/\s{2,}/g, ' ');
+    address = decode(address.replace(/\s{2,}/g, ' ').trim());
 
     let menuItems = $('.rest-menu > p', restaurant)
       .html()
@@ -22,7 +27,9 @@ const parseHTML = async ({ data }, weekDay) => {
 
     if (menuItems) {
       // remove whitespace
-      menuItems = menuItems.map(menuItem => menuItem.replace(/\s{2,}/g, ' '));
+      menuItems = menuItems.map(menuItem =>
+        decode(menuItem.replace(/\s{2,}/g, ' ').trim())
+      );
 
       restaurants.push({
         title,
