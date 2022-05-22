f5a7973cfcaa9b4f3fd435ee7e5d0dc5bff36748
Patrik Elfström <me@patrikelfstrom.se>
2022-05-22 15:55:17 +0200

    Added trpc

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import type { Component } from "solid-js";
+import { Component } from "solid-js";
+import { createTrpcQuery } from "./lib/trpc";
 
 const App: Component = () => {
+  const date = new Date();
+  const [data] = createTrpcQuery("hello", { text: "world" });
+
   return (
     <>
       <h1>Schnitzellunch.se</h1>
+      {data()?.greeting ?? "Loading..."}
     </>
   );
 };
