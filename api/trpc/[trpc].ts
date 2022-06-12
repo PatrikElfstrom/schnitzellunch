095832854162c401ab1d1c7432c4c03e71ee300d
Patrik Elfström <me@patrikelfstrom.se>
2022-06-13 00:40:35 +0200

    Added caching

--- api/trpc/[trpc].ts
+++ api/trpc/[trpc].ts
@@ -1,8 +1,25 @@
-import { router } from "@trpc/server";
-import { createNextApiHandler } from "@trpc/server/adapters/next";
+import { inferAsyncReturnType, router } from "@trpc/server";
+import {
+  createNextApiHandler,
+  CreateNextContextOptions,
+} from "@trpc/server/adapters/next";
 import { restaurants } from "../lib/routers/_restaurants";
 
-const appRouter = router().merge(restaurants);
+const createContext = async ({ req, res }: CreateNextContextOptions) => {
+  return {
+    req,
+    res,
+    prisma,
+  };
+};
+
+type Context = inferAsyncReturnType<typeof createContext>;
+
+function createRouter() {
+  return router<Context>();
+}
+
+const appRouter = createRouter().merge(restaurants);
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
@@ -10,5 +27,26 @@ export type AppRouter = typeof appRouter;
 // export API handler
 export default createNextApiHandler({
   router: appRouter,
-  createContext: () => null,
+  createContext,
+  responseMeta({ ctx, paths, type, errors }) {
+    // assuming you have all your public routes with the keyword `public` in them
+    const allPublic =
+      paths && paths.every((path) => path.includes("restaurants"));
+    // checking that no procedures errored
+    const allOk = errors.length === 0;
+    // checking we're doing a query request
+    const isQuery = type === "query";
+
+    console.log(ctx);
+    if (ctx?.res && allPublic && allOk && isQuery) {
+      // cache request for 1 day + revalidate once every second
+      const ONE_DAY_IN_SECONDS = 60 * 60 * 24;
+      return {
+        headers: {
+          "cache-control": `s-maxage=1, stale-while-revalidate=${ONE_DAY_IN_SECONDS}`,
+        },
+      };
+    }
+    return {};
+  },
 });
